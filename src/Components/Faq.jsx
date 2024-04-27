0d976ae177addcf60d00d96478c0996ce2697e80
saiyedimtiaj <hridoyemtiaz0@gmail.com>
2024-04-27 13:08:27 +0600

    some change in functionality

--- src/Components/Faq.jsx
+++ src/Components/Faq.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import { useState } from "react";
-import { FaChevronDown,FaChevronUp } from "react-icons/fa";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 
 const Faq = () => {
-    const [isOpen, setIsOpen] = useState(null);
+  const [isOpen, setIsOpen] = useState(null);
 
-    const handleToggle = (idx) => setIsOpen((prevIdx) => (prevIdx === idx ? null : idx));
+  const handleToggle = (idx) => {
+    setIsOpen((prevIdx) => (prevIdx === idx ? null : idx));
+  };
 
-    const datas = [
-        { title: 'Connected Devices', color: 'green', description: 'To create an account, click on the "Sign Up" button and fill out the required information. Once done, you can enjoy the benefits of being a registered member.'},
-        { title: 'Apps Permissions', color: 'sky', description: 'Our return policy allows you to return items within 30 days of purchase. Please visit our returns page for detailed instructions and to initiate a return.'},
-        { title: 'Pending task', color: 'purple', description: 'Yes, you can change your shipping address before your order is shipped. Go to your account settings and update the shipping information accordingly.'},
-    ];
+  const datas = [
+    {
+      title: "Can education flashcards be used for all age groups?",
+      description:
+        'Yes, education flashcards can be tailored to different age groups and learning levels. There are flashcards designed for preschoolers, elementary school students, high school students, and even for college-level and adult learners.',
+    },
+    {
+      title: "How do education flashcards work?",
+      description:
+        "Can education flashcards be used for test preparation?",
+    },
+    {
+      title: "Pending task",
+      description:
+        "Yes, you can change your shipping address before your order is shipped. Go to your account settings and update the shipping information accordingly.",
+    },
+  ];
 
   return (
     <div className="mb-14">
@@ -20,28 +34,29 @@ const Faq = () => {
           WebkitBackgroundClip: "text",
           WebkitTextFillColor: "transparent",
         }}
-        className="bg-gradient-to-r font-semibold from-indigo-700 to-indigo-500 text-3xl"
+        className="bg-gradient-to-r font-semibold from-indigo-700 to-indigo-500 text-3xl mb-6"
       >
         FAQ
       </h1>
-      <div className="mt-6">
+      <div>
         {datas.map((data, idx) => (
           <div key={idx} className="mb-4">
-            <div
-              className={`flex items-center max-w-[650px] justify-between p-4 rounded-lg border border-indigo-600`}
-            >
-              <div>
-               <div className="flex items-center justify-between">
-               <h2 className="text-lg font-semibold">{data.title}</h2>
-               </div>
-                {isOpen === idx && <p className="mt-2 text-gray-600">{data.description}</p>}
+            <div className={`max-w-[750px] rounded-lg border border-indigo-600 p-4 transition-all duration-300 ease-in-out ${isOpen === idx ? 'bg-gray-100' : ''}`}>
+              <div onClick={() => handleToggle(idx)} className="flex cursor-pointer items-center justify-between">
+                <h2 className="text-lg font-semibold">{data.title}</h2>
+                <button
+                  className={`p-2 rounded-full text-black focus:outline-none`}
+                >
+                  {isOpen === idx ? (
+                    <FaChevronUp size={20} />
+                  ) : (
+                    <FaChevronDown size={20} />
+                  )}
+                </button>
               </div>
-              <button
-                className={`ml-4 p-2 rounded-full bg-${data.color}-400 text-black focus:outline-none`}
-                onClick={() => handleToggle(idx)}
-              >
-                {isOpen === idx ? <FaChevronUp  size={20} /> : <FaChevronDown size={20} />}
-              </button>
+              {isOpen === idx && (
+                <div className="mt-2 text-gray-600">{data.description}</div>
+              )}
             </div>
           </div>
         ))}
--- src/Components/Tabs.jsx
+++ src/Components/Tabs.jsx
@@ -5,39 +5,19 @@ import { IoReload } from "react-icons/io5";
 import { MdFullscreen } from "react-icons/md";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 import publishLogo from "../assets/Frame 41.png";
+import ReactCardFlip from "react-card-flip";
 
 const Tabs = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [isFlipped, setIsFlipped] = useState(false);
 
-  const data = [
-    { math: "9 + 6 + 7x + 2x + 8" },
-    { math: "3 + 8 + 7x + 2x + 4" },
-    { math: "5 + 6 + 3x + 5x + 6" },
-    { math: "2 + 3x + 4 + 5x + 6" },
-    { math: "4 + 5x + 6 + 7x + 8" },
-    { math: "6 + 7x + 8 + 9x + 10" },
-    { math: "8 + 9x + 10 + 11x + 12" },
-    { math: "10 + 11x + 12 + 13x + 14" },
-    { math: "12 + 13x + 14 + 15x + 16" },
-    { math: "14 + 15x + 16 + 17x + 18" }
-  ];
-
-  const nextEquation = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
-  };
-
-  const prevEquation = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + data.length) % data.length);
-  };
-
-  const reloadEquation = () => {
-    setCurrentIndex(0); 
+  const handleClick = (e) => {
+    e.preventDefault();
+    setIsFlipped(!isFlipped);
   };
 
   return (
     <div>
-      <div className="flex gap-4 md:gap-6 items-center justify-center flex-wrap text-sm md:text-lg">
+      <div className="flex mb-7 gap-4 md:gap-6 items-center justify-center flex-wrap text-sm md:text-lg">
         <span className="text-indigo-600 cursor-pointer font-semibold">
           Study
         </span>
@@ -47,51 +27,61 @@ const Tabs = () => {
         <span className="text-gray-500 cursor-pointer font-medium">Other</span>
       </div>
       <div>
-        <div className={`${isFullScreen ? 'w-full' : 'max-w-[550px]'} rounded-xl mx-auto mt-7 bg-gradient-to-r from-indigo-700 to-indigo-500`}>
-          <div className=" flex items-center text-white px-4 justify-between py-4">
-            <span>
-              <FaRegLightbulb size={23} />
-            </span>
-            <span>
-              <AiOutlineSound size={23} />
-            </span>
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+          <div
+          onClick={handleClick}
+            className={`max-w-[550px] rounded-xl mx-auto bg-gradient-to-r from-indigo-700 to-indigo-500`}
+          >
+            <div className=" flex items-center text-white px-4 justify-between py-4">
+              <span>
+                <FaRegLightbulb size={23} />
+              </span>
+              <span>
+                <AiOutlineSound size={23} />
+              </span>
+            </div>
+            <div className="text-center pt-28 pb-32">
+              <h1 className="text-2xl font-semibold text-white">9 + 6 + 7x -2x -3</h1>
+            </div>
           </div>
-          <div className="text-center pt-28 pb-32">
-            <h1 className="text-2xl font-semibold text-white">
-              {data[currentIndex]?.math}
-            </h1>
+
+          <div
+            onClick={handleClick}
+            className={`max-w-[550px] rounded-xl mx-auto bg-gradient-to-r from-gray-700 to-gray-500`}
+          >
+            <div className=" flex items-center text-white px-4 justify-between py-4">
+              <span>
+                <FaRegLightbulb size={23} />
+              </span>
+              <span>
+                <AiOutlineSound size={23} />
+              </span>
+            </div>
+            <div className="text-center pt-28 pb-32">
+              <h1 className="text-2xl font-semibold text-white">5x + 12</h1>
+            </div>
           </div>
-        </div>
+        </ReactCardFlip>
+
         <div className="flex max-w-[500px] gap-4 px-5 mx-auto justify-between items-center pt-4 pb-8">
-          <span className="text-indigo-600 cursor-pointer font-bold" onClick={reloadEquation}>
+          <span className="text-indigo-600 cursor-pointer font-bold">
             <IoReload size={21} />
           </span>
           <div className="flex gap-3 items-center">
-            <button
-              className="bg-gradient-to-r from-indigo-700 to-indigo-500 p-2 rounded-full text-white"
-              onClick={prevEquation}
-            >
+            <button className="bg-gradient-to-r from-indigo-700 to-indigo-500 p-2 rounded-full text-white">
               <FaAngleLeft />
             </button>
-            <span className="font-semibold">
-              {currentIndex + 1} / {data.length}
-            </span>
-            <button
-              className="bg-gradient-to-r from-indigo-700 to-indigo-500 p-2 rounded-full text-white"
-              onClick={nextEquation}
-            >
+            <span className="font-semibold">1 / 10</span>
+            <button className="bg-gradient-to-r from-indigo-700 to-indigo-500 p-2 rounded-full text-white">
               <FaAngleRight />
             </button>
           </div>
-          <span
-            onClick={() => setIsFullScreen(!isFullScreen)}
-            className="text-indigo-600 cursor-pointer font-bold"
-          >
+          <span className="text-indigo-600 cursor-pointer font-bold">
             <MdFullscreen size={23} />
           </span>
         </div>
       </div>
-      <div className="flex items-center justify-between py-8">
+      <div className="flex items-center justify-between my-8">
         <img src={publishLogo} className="md:h-20 h-12" alt="" />
         <button className="flex gap-2 items-center">
           <span className="bg-gradient-to-r text-xs from-indigo-700 to-indigo-500 p-2 rounded-full text-white">
